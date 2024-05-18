f3e1e2f14fabc9d4079731da09b3d3c789cd7581
Jesse Nay <jessenay26@gmail.com>
2024-05-18 16:23:18 -0600

    trying to fix database connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,5 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://jessenay26:LD1tpN5bZXuCw1et@cluster0.knlwguy.mongodb.net/LiftsDB?retryWrites=true&w=majority&appName=Cluster0');
-
+mongoose.connect(process.env.MONGODB_URI);
 module.exports = mongoose.connection;
